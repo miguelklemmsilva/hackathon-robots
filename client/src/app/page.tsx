b7c29a4f29519decba02532fc85f7afd5b61b49b
Aliff Amir <aliff6406@gmail.com>
2025-02-09 04:31:08 +0000

    connect client to ws api

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,9 +1,45 @@
-import Image from "next/image";
+"use client";
+
+import { useEffect, useState } from "react";
+import useWebSocket, { ReadyState } from "react-use-websocket";
 
 export default function Home() {
+  //Public API that will echo messages sent to it back to the client
+  const [socketUrl, setSocketUrl] = useState(
+    process.env.NEXT_PUBLIC_WEBSOCKET_URL
+  );
+  const [messageHistory, setMessageHistory] = useState<MessageEvent<any>[]>([]);
+
+  const { sendMessage, lastMessage, readyState } = useWebSocket(socketUrl!);
+
+  useEffect(() => {
+    if (lastMessage !== null) {
+      setMessageHistory((prev) => prev.concat(lastMessage));
+    }
+  }, [lastMessage]);
+
+  const connectionStatus = {
+    [ReadyState.CONNECTING]: "Connecting",
+    [ReadyState.OPEN]: "Open",
+    [ReadyState.CLOSING]: "Closing",
+    [ReadyState.CLOSED]: "Closed",
+    [ReadyState.UNINSTANTIATED]: "Uninstantiated",
+  }[readyState];
+
   return (
-    <div className="">
-      <main className="">Hello World</main>
+    <div>
+      <main>
+        <h1>Spot Client</h1>
+        <div>
+          <span>The WebSocket is currently {connectionStatus}</span>
+          {lastMessage ? <span>Last message: {lastMessage.data}</span> : null}
+          <ul>
+            {messageHistory.map((message, idx) => (
+              <span key={idx}>{message ? message.data : null}</span>
+            ))}
+          </ul>
+        </div>
+      </main>
     </div>
   );
 }
